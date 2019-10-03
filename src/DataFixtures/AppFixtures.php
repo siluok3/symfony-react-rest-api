266e4f7c52173c86d4a34852992659a778c71240
Kiriakos Papachristou <kiriakos.papachristou@trivago.com>
2019-10-03 12:34:02 +0200

    Fix fixtures with relations and add password encrypter

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,19 +3,37 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /** @var UserPasswordEncoderInterface */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('kiri_author');
+
         $blogPost =  new BlogPost();
         $blogPost->setTitle('Turing');
         $blogPost->setPublished(new \DateTime('2019-09-15 09:00:00'));
         $blogPost->setSlug('hey-you');
         $blogPost->setContent('That is a content');
-        $blogPost->setAuthor('Kiri Pap');
+        $blogPost->setAuthor($user);
 
         $manager->persist($blogPost);
 
@@ -24,10 +42,29 @@ class AppFixtures extends Fixture
         $blogPost->setPublished(new \DateTime('2019-09-16 09:30:00'));
         $blogPost->setSlug('Que hace?');
         $blogPost->setContent('That is a content for ancient Greeks');
-        $blogPost->setAuthor('siluok3');
+        $blogPost->setAuthor($user);
 
         $manager->persist($blogPost);
 
+        $manager->flush();
+    }
+
+    public function loadComments(){}
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('siluok3');
+        $user->setEmail('kiripap@gmail.com');
+        $user->setName('Kiriakos Papachristou');
+        $user->setPassword($this->encoder->encodePassword(
+            $user,
+            'verysafepassword'
+        ));
+
+        $this->addReference('kiri_author', $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
