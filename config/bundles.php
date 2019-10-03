8c51bdefa1017e225f52602ca13901671eb2b23c
Kiriakos Papachristou <kiriakos.papachristou@trivago.com>
2019-10-03 10:52:15 +0200

    Add symfony web server bundle for local dev

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
